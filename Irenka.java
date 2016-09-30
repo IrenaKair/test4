cc658834750242fc384289db29fe41e53ff72b30
IrenaKair <irenakaczmarek@wp.pl>
2016-09-30 18:38:01 +0200

    Update Irenka.java

--- Irenka.java
+++ Irenka.java
@@ -4,5 +4,6 @@
 public class Irenka {
     public static void main ( String []args){
         System.out.println(" Cześc kochanie jak to widzisz to jest OK. Kocham Cię");
+        System.out.println(" Cześć!  Idziemy...? ");
     }
 }
