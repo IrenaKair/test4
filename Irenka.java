9c9f74050c2953b7ee7229021ce9301bc3909990
Robertk13 <kaczmarekrob13@gmail.com>
2016-09-30 18:26:23 +0200

    dodanie pliku irenka

---
+++ Irenka.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Robert on 2016-09-30.
+ */
+public class Irenka {
+    public static void main ( String []args){
+        System.out.println(" Cześc kochanie jak to widzisz to jest OK. Kocham Cię");
+    }
+}
